c5077aae1cff0cba7b5c90b124092b11ad640853
saminulh <saminulh@hotmail.com>
2016-12-14 23:48:50 -0500

    Revert "upload"
    This reverts commit 459c7ab97310540ff728a61f861d190fa6aeea36.

--- Current Build/Assets/Ability Info Containers/AbilityInfoContainer.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-using System;
-
-public abstract class AbilityInfoContainer {
-	public delegate bool Del(AbilityInfoContainer info);
-	public Del abilityFunction; 
-}
